713fcbe9b73ea3f206707e4deff313cd8b183315
Carole <cjpillette@hotmail.com>
2016-11-25 15:43:27 +1100

    Add newMessage and createMessage event

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function () {
+	console.log('Connected to Server');
+
+	socket.emit('createMessage', {
+		from: 'jen',
+		text: 'hey this is carole'
+	});
+});
+
+socket.on('disconnect', function () {
+	console.log('Disconnected to Server');
+});
+
+socket.on('newMessage', function (message) {
+	console.log('New message', message);
+})
